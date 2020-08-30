37dd932947cd12b2f09a82de0ef14aa1ea2036f2
Capta1nCoCo <femabu28@gmail.com>
2020-08-31 01:03:54 +0300

    89. ViewportToWorldPoint()

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -6,11 +6,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
-
-    // Start is called before the first frame update
+    [SerializeField] float padding = 0.5f;
+    
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+        
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -23,8 +37,13 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        
         var newXPos = transform.position.x + deltaX;
         var newYPos = transform.position.y + deltaY;
-        transform.position = new Vector2(newXPos, newYPos);                        
+
+        float clampedXPos = Mathf.Clamp(newXPos, xMin, xMax);
+        float clampedYPos = Mathf.Clamp(newYPos, yMin, yMax);
+
+        transform.position = new Vector2(clampedXPos, clampedYPos);                        
     }
 }
