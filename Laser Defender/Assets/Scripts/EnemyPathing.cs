f50a5435b726b103af0399457a903eaf14ff3cd9
Capta1nCoCo <femabu28@gmail.com>
2020-08-31 19:51:13 +0300

    98. Using A Foreach Loop

--- Laser Defender/Assets/Scripts/EnemyPathing.cs
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;    
     [SerializeField] float moveSpeed = 2f;
+    
+    List<Transform> waypoints;
     int waypointIndex = 0;
 
     private void Start()
     {
+        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
     }
 
--- Laser Defender/Assets/Scripts/WaveConfig.cs
+++ Laser Defender/Assets/Scripts/WaveConfig.cs
@@ -13,7 +13,15 @@ public class WaveConfig : ScriptableObject
     [SerializeField] float moveSpeed = 2f;
 
     public GameObject GetEnemyPrefab() { return enemyPrefab; }
-    public GameObject GetPathPrefab() { return pathPrefab; }
+    public List<Transform> GetWaypoints() 
+    {
+        var waveWaypoints = new List<Transform>();
+        foreach (Transform child in pathPrefab.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints;
+    }
     public float GetTimeBetweenSpawns() { return timeBetweenSpawns; }    
     public float GetSpawnRandomFactor() { return spawnRandomFactor; }
     public int GetNumberOfEnemies() { return numberOfEnemies; }
