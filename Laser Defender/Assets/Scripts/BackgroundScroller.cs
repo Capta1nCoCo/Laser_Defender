fdab849e7a953fdf2893901eac8c9b25274dcee7
Capta1nCoCo <femabu28@gmail.com>
2020-09-01 19:49:25 +0300

    108. Scrolling Background

---
+++ Laser Defender/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    Material myMaterial;
+    Vector2 offSet;
+
+    void Start()
+    {
+        myMaterial = GetComponent<MeshRenderer>().material;
+        offSet = new Vector2(0, backgroundScrollSpeed);
+    }
+
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
