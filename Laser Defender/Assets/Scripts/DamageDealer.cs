9f07f938e45a5642178a5a0c5f295370a0a63c66
Capta1nCoCo <femabu28@gmail.com>
2020-09-01 15:31:22 +0300

    103. Create Damage Dealer Class

---
+++ Laser Defender/Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 100;
+
+    public int GetDamage() { return damage; }
+
+    public void ProcessHit()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Laser Defender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        health -= damageDealer.GetDamage();
+    }
+
+}
