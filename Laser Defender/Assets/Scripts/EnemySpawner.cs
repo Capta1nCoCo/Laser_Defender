b8e8f81bd8fa1e1da535f8c40b57d988c3d6c4f0
Capta1nCoCo <femabu28@gmail.com>
2020-09-01 14:51:01 +0300

    101. Spawn Multiple Enemy Waves

--- Laser Defender/Assets/Scripts/EnemySpawner.cs
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfig> waveConfigs;
-    int startingWave = 0;
+    [SerializeField] int startingWave = 0;
 
     void Start()
     {
-        var currentWave = waveConfigs[startingWave];
-        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        StartCoroutine(SpawnAllWaves());
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+        {
+            var currentWave = waveConfigs[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        }
     }
 
     private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
