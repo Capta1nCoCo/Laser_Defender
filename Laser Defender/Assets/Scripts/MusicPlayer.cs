76c6938d9a1a4fdfb5f2c82a612de5a91b482b79
Capta1nCoCo <femabu28@gmail.com>
2020-09-02 19:35:08 +0300

    114. Music Player With Singleton

---
+++ Laser Defender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
